ca6029d24aa898682a290b52e66b4d5296694173
ytshen <ytshen@ytshenmac.dyndns.cern.ch>
2016-11-15 14:29:20 +0100

    v01. First commit.
    The current version should work and was testing using v44 4topSM sample.
    But I have to run over all v44 bkg samples and compare with Ximo's
    results to make sure the results are correct.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NUHM2
 This is the framework for NUHM2 SR optimization.
+
+v01. First commit.
+The current version should work and was testing using v44 4topSM sample.
+But I have to run over all v44 bkg samples and compare with Ximo's results to make sure the results are correct. 
---
+++ ytNUHM2Analysis/Root/LinkDef.h
@@ -0,0 +1,18 @@
+#include <ytNUHM2Analysis/ytEventSelection.h>
+#include <ytNUHM2Analysis/yt_cutflows.h>
+#include <ytNUHM2Analysis/yt_optimization.h>
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+
+#endif
+
+#ifdef __CINT__
+#pragma link C++ class ytEventSelection+;
+#pragma link C++ class yt_cutflows+;
+#pragma link C++ class yt_optimization+;
+#endif
